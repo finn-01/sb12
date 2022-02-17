11917c54d7dbe1c675f0715773cbc59660404d2f
finn-01 <nguyensam576@gmail.com>
2022-02-17 21:55:25 +0700

    set up

---
+++ src/main/java/vn/finn/spring/demospringjpaquery/DemoSpringJpaQueryApplication.java
@@ -0,0 +1,13 @@
+package vn.finn.spring.demospringjpaquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringJpaQueryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringJpaQueryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/finn/spring/demospringjpaquery/DemoSpringJpaQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.finn.spring.demospringjpaquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringJpaQueryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
